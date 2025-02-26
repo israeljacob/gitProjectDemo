a92446dbe0582335d7c53c15ea7e6f7a12fe5a41
israelr9897 <admin@admins-MacBook-Pro.local>
2025-02-26 23:45:11 +0200

    menu

--- user/MachinesManager.js
+++ user/MachinesManager.js
@@ -62,8 +62,9 @@ function addListToElement(machinesList, ownersList) {
         computerDiv.className = "computer";
         computerDiv.innerHTML = `
             <img src="https://img.icons8.com/ios-filled/100/58a6ff/computer.png" alt="computer icon">
-            <div class="computer-info">computer ${i + 1}</div>
-            <div class="computer-info">owner: ${machinesList[i]}</div>
+            
+            <div class="computer-info">machine ${i + 1}</div>
+            <div class="computer-info">User: ${machinesList[i]}</div>
             <div class="computer-info">MAC: ${ownersList[i]}</div>
         `;
         computerDiv.addEventListener('click', () => { getMachineData(computerDiv, machinesList[i]); });
